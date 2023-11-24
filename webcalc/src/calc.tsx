0220f300417a863b1338239dfc31a0649a4c2589
Timjapri <timothy.japri@binus.ac.id>
2023-11-24 18:16:58 +0700

    working button

--- webcalc/src/calc.tsx
+++ webcalc/src/calc.tsx
@@ -1,7 +1,11 @@
 import './style.css';
 
 function calc() {
-  
+
+
+  const handleButtonClick = (value) => {
+    setCurrentInput((prevInput) => prevInput + value);
+  };
 
   return (
     <div className="App">
@@ -14,70 +18,70 @@ function calc() {
         <div className="top">
           <div className="cleft">
             <div className="row">
-              <button type="submit" id="clear" className="graybut">
+              <button type="button" id="clear" className="graybut">
                 C
               </button>
-              <button type="submit" id="delete" className="graybut">
+              <button type="button" id="delete" className="graybut">
                 Del
               </button>
-              <button type="submit" id="help" className="browbut">
+              <button type="button" id="help" className="browbut">
                 ?
               </button>
             </div>
             <div className="row">
-              <button type="submit" id="one" className="graybut">
+              <button type="button" onClick={() => handleButtonClick('1')} id="one" className="graybut">
                 1
               </button>
-              <button type="submit" id="two" className="graybut">
+              <button type="button" onClick={() => handleButtonClick('2')} id="two" className="graybut">
                 2
               </button>
-              <button type="submit" id="three" className="graybut">
+              <button type="button" onClick={() => handleButtonClick('3')} id="three" className="graybut">
                 3
               </button>
             </div>
             <div className="row">
-              <button type="submit" id="four" className="graybut">
+              <button type="button" onClick={() => handleButtonClick('4')} id="four" className="graybut">
                 4
               </button>
-              <button type="submit" id="five" className="graybut">
+              <button type="button" onClick={() => handleButtonClick('5')} id="five" className="graybut">
                 5
               </button>
-              <button type="submit" id="six" className="graybut">
+              <button type="button" onClick={() => handleButtonClick('6')} id="six" className="graybut">
                 6
               </button>
             </div>
             <div className="row">
-              <button type="submit" id="seven" className="graybut">
+              <button type="button" onClick={() => handleButtonClick('7')} id="seven" className="graybut">
                 7
               </button>
-              <button type="submit" id="eight" className="graybut">
+              <button type="button" onClick={() => handleButtonClick('8')} id="eight" className="graybut">
                 8
               </button>
-              <button type="submit" id="nine" className="graybut">
+              <button type="button" onClick={() => handleButtonClick('9')} id="nine" className="graybut">
                 9
               </button>
             </div>
           </div>
           <div className="cright">
-            <button type="submit" id="divide" className="yelbut">
+            <button type="button" onClick={() => handleButtonClick('')} id="divide" className="yelbut">
               /
             </button>
-            <button type="submit" id="multiply" className="yelbut">
+            <button type="button" onClick={() => handleButtonClick('')} id="multiply" className="yelbut">
               x
             </button>
-            <button type="submit" id="minus" className="yelbut">
+            <button type="button" onClick={() => handleButtonClick('')} id="minus" className="yelbut">
               -
             </button>
-            <button type="submit" id="add" className="yelbut">
+            <button type="button" onClick={() => handleButtonClick('')} id="add" className="yelbut">
               +
             </button>
           </div>
         </div>
         <div className="bottom">
-          <button type="submit" id="zero" className="graybut">
+          <button type="button" onClick={() => handleButtonClick('0')} id="zero" className="graybut">
             0
           </button>
-          <button type="submit" id="equal" className="yelbut">
+          <button type="button" id="equal" className="yelbut">
             =
           </button>
         </div>
