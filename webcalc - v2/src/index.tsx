e0f97ff11a31021d43a0deb8026a402b9fcd19b7
Timjapri <timothy.japri@binus.ac.id>
2023-12-07 04:44:33 +0700

    no change

--- webcalc - v2/src/index.tsx
+++ webcalc - v2/src/index.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Calc from './components/calc';
-import Support from './components/support';
+import Calc from './calc';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/Support" element={<Support />} />
-      <Route path="/" element={<Calc />} />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <Calc />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- webcalc/src/index.tsx
+++ webcalc/src/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Calc from './calc';
+import Calc from './components/calc';
+import Support from './components/support';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Calc />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/Support" element={<Support />} />
+      <Route path="/" element={<Calc />} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
